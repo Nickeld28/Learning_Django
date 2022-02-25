eaef2db4f32bb93accd213786122ec123d92b927
Nickolay Dobrodey <dobrodeynickolay@gmail.com>
2022-02-26 01:25:21 +0300

    Создана модель Entry, выполнена миграция и регистрация на административном сайте. Добавлено 3 записи в созданные ранее темы.

--- learning_log/learning_logs/admin.py
+++ learning_log/learning_logs/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Topic  # Точка сообщает Django, что файл models.py следует искать в одном каталоге с admin.py
+# Точка сообщает Django, что файл models.py следует искать в одном каталоге с admin.py
+from .models import Topic, Entry
 
-admin.site.register(Topic)  # сообщает Django, что управление моделью должно осуществляться через административный сайт
+# сообщает Django, что управление моделью должно осуществляться через административный сайт
+admin.site.register(Topic)
+admin.site.register(Entry)
