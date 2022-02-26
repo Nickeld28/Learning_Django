c7d7d3a005847559a199e1febd23788392fc7876
Nickolay Dobrodey <dobrodeynickolay@gmail.com>
2022-02-26 20:40:04 +0300

    Создал домашнюю страницу проекта learning log. Создал файл urls.py для приложения, изменил файл views.py, создал файл шаблона index.html. Перезапустил сервер проекта, убедился, что домашняя страница отображается корректно.

--- learning_log/learning_log/urls.py
+++ learning_log/learning_log/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Добавляем строку включения модуля learning_logs.urls.
+    # После этого нужно создать второй файл urls.py в папке learning_logs.
+    path('', include('learning_logs.urls')),
 ]
---
+++ learning_log/learning_logs/urls.py
@@ -0,0 +1,16 @@
+""" Определяет схемы URL для learning_logs """  # строка документации показывает с какой версией urls.py работаем
+
+# импортируется функция path для связывания URL с представлениями (views)
+from django.urls import path
+
+# точка указывает на то, что Python должен импортировать views из каталога, в котором находится текущий модуль urls.py
+from . import views
+
+# переменная app_name помогает django отличить этот файл urls.py от одноименных файлов в других приложениях
+app_name = 'learning_logs'
+
+# переменная urlpatterns - это список страниц, которые могут запрашиваться из приложения learning_logs
+urlpatterns = [
+    # Домашняя страница
+    path('', views.index, name='index'),
+]
