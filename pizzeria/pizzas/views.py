1c25d94f63af88629644bd1acd06ec0a65a34bd3
Nickolay Dobrodey <dobrodeynickolay@gmail.com>
2022-02-27 00:41:41 +0300

    Выполнил упражнение 18.6: Создал файл urls.py в pizzeria/pizzas. Внес изменения в файл urls.py в папке pizzeria. Внес изменения в файл views.py. В pizzas создал каталог templates, а внутри создал еще каталог pizzas, а в нем файл index.html. Запустил сервер, убедился в отображении домашней страницы.

--- pizzeria/pizzas/views.py
+++ pizzeria/pizzas/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """ Домашняя страница приложения pizzeria """
+    return render(request, 'pizzas/index.html')
