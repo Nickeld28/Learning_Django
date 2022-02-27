8518625d40d8252393283f35a0a33537510138cd
Nickolay Dobrodey <dobrodeynickolay@gmail.com>
2022-02-27 19:35:10 +0300

    Выполнил упражнение 18.7 и упражднение 18.8. Изменил схему URL pizzas/urls.py. Добавил представление для отображения списка видов пиццы и топпингов для каждого вида пиццы, применив наследование шаблонов. Добавил файлы base.html, pizzas.html, pizza.html. Убедился, что на сайте отображаются внесенные изменения.

--- pizzeria/pizzas/urls.py
+++ pizzeria/pizzas/urls.py
@@ -11,4 +11,10 @@ app_name = 'pizzas'
 urlpatterns = [
     # Домашняя страница
     path('', views.index, name='index'),
+
+    # Страница со списком всех видов пиццы
+    path('pizzas/', views.pizzas, name='pizzas'),
+
+    # Страница с подробной информацией по отдельной пицце
+    path('pizza/<int:pizza_id>/', views.pizza, name='pizza'),
 ]
--- pizzeria/pizzas/views.py
+++ pizzeria/pizzas/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
+from .models import Pizza
+
 
 def index(request):
     """ Домашняя страница приложения pizzeria """
     return render(request, 'pizzas/index.html')
+
+
+def pizzas(request):
+    """ Выводит список видов пиццы """
+    pizzas = Pizza.objects.order_by('name')
+    context = {'pizzas': pizzas}
+    return render(request, 'pizzas/pizzas.html', context)
+
+
+def pizza(request, pizza_id):
+    """ Выводит одну пиццу и все ее топпинги """
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by('name')
+    context = {'pizza': pizza, 'toppings': toppings}
+    return render(request, 'pizzas/pizza.html', context)
