094e51da67a8bce5c834f2495f8b67d84c136cd1
Nickolay Dobrodey <dobrodeynickolay@gmail.com>
2022-02-27 13:37:40 +0300

    Создал страницу со списком тем. Изменил схема URL в learning_logs/urls.py. Изменил файл views.py. Создал шаблон topics.html и изменил шаблон base.html. Запумтил сервер чтобы убедиться в том, что необходимые изменения отображались на сайте.

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -13,4 +13,7 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Домашняя страница
     path('', views.index, name='index'),
+
+    # Страница со списком всех тем
+    path('topics/', views.topics, name='topics'),
 ]
