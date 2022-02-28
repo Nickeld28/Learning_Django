7aa8e54bcf120e0c9b5e5e459d4083b1cb61c64b
Nickolay Dobrodey <dobrodeynickolay@gmail.com>
2022-03-01 01:23:36 +0300

    Для проекта Blog добавил поле для добавления новых постов, и добавил поля редактирования для каждого поста. Название поста на домашней страницей сделал ссылкой на страницу с постом. Убедился что поля работают как надо.

--- Blog/blogs/urls.py
+++ Blog/blogs/urls.py
@@ -9,4 +9,13 @@ app_name = 'blogs'
 urlpatterns = [
     # Домашняя страница
     path('', views.index, name='index'),
+
+    # Страница для редактирования записи
+    path('new_post/', views.new_post, name='new_post'),
+
+    # Страница с подробной информацией о посте
+    path('post/<int:post_id>/', views.post, name='post'),
+
+    # Страница для редактирования записи
+    path('edit_post/<int:post_id>/', views.edit_post, name='edit_post'),
 ]
--- Blog/blogs/views.py
+++ Blog/blogs/views.py
@@ -1,8 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .forms import PostForm
 from .models import BlogPost
+
 
 def index(request):
     """ Домашняя страница приложения blogs """
     posts = BlogPost.objects.order_by('date_added')
     return render(request, 'blogs/index.html', {'posts': posts})
+
+
+def new_post(request):
+    """Добавляет новый пост """
+    if request.method != 'POST':
+        # Данные не отправлялись; создается пустая форма.
+        form = PostForm()
+
+    else:
+        # Отправлены данные POST; обработать данные.
+        form = PostForm(data=request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('blogs:index')
+    # Вывести пустую или недействительную форму.
+    context = {'form': form}
+    return render(request, 'blogs/new_post.html', context)
+
+
+def edit_post(request, post_id):
+    """ Редактирует существующий пост """
+    post = BlogPost.objects.get(id=post_id)
+    if request.method != 'POST':
+    # Исходный запрос; форма заполняется данными текущей записи.
+        form = PostForm(instance=post)
+    else:
+        # Отправка данных POST; обработать данные.
+        form = PostForm(instance=post, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blogs:index')
+    context = {'post': post, 'form': form}
+    return render(request, 'blogs/edit_post.html', context)
+
+
+def post(request, post_id):
+    """ Выводит один пост """
+    post = BlogPost.objects.get(id=post_id)
+    context = {'post': post}
+    return render(request, 'blogs/post.html', context)
