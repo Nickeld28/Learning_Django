8d05350bd68de95993235d53973de4ae376db7a3
Nickolay Dobrodey <dobrodeynickolay@gmail.com>
2022-03-01 20:00:39 +0300

    В проекте Learning Log сделал страницу регистрации. Дописал изменения в файлы users/urls.py и users\views.py. Создал шаблон регистрации ~\users\templates\users\register.html и исправил родительский шаблон base.html, добавив ссылку регистрации. В процессе возникла ошибка, подробности я сохранил в файле .html в папке Errors, попутно решив проблему с Line Separator (LF --> CRLF). Как именно исправил ошибку, подробно описал в Памятка действий.txt. Создал нового пользователя, убедился что работает login и logout с учетными данными нового пользователя.

--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -2,8 +2,13 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
     # Включить URL авторизацию по умолчанию
     path('', include('django.contrib.auth.urls')),
+
+    # Страница регистрации
+    path('register/', views.register, name='register'),
 ]
